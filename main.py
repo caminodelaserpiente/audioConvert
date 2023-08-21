3feb88dad85d2a142d5e9570e236291fee05e5a7
蛇道 <caminodelaserpiente.py@gmail.com>
2023-08-21 17:38:55 -0600

    [ADD] Launcher Application

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+Author: 蛇道
+Url: https://github.com/caminodelaserpiente
+"""
+
+
+from audioConvert import audioConvert
+
+
+def main():
+    audioConvert()
+
+
+if __name__ == "__main__":
+    main()
